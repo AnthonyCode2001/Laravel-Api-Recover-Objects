d43230d8bf42a112d48614bd9060ce4342b46e7c
Anthony Ledezma <ledezmaanthonydeveloper@gmail.com>
2022-07-08 11:11:15 -0500

    migrate table solicitudes

---
+++ app/Models/Solicitudes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Solicitudes extends Model
+{
+    use HasFactory;
+    protected $fillable = ['cedula', 'nombre_objeto', 'color', 'price', 'detalles', 'tipo_objeto'];
+}
